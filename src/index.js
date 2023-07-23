ef791cf024de522f8a1fcc501b3a51b390a193ee
Bharath Gowda P <bharathbg882@gmail.com>
2023-07-23 14:46:49 +0530

    added vercel analytics

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
+        <Analytics/>
         <App classname="m-0 p-0"/>
     </BrowserRouter>
 );
